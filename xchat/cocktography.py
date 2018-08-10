d9d98cc2b52e06aebc75ec8a6a87d4d4b106285a
fidsah <vin.king@gmail.com>
2018-08-10 14:56:16 -0400

    Modified for XChat, tested, ported chainsol changes
    Resolves #7

--- xchat/cocktography.py
+++ xchat/cocktography.py
@@ -4,12 +4,12 @@ import itertools
 import base64
 import StringIO
 import regex as re
-import xchat
+import hexchat
 import os
 
 __module_name__ = str("cocktography")
-__module_version__ = str("1.1")
-__module_description__ = str("Cocktographical library for xchat")
+__module_version__ = str("1.2")
+__module_description__ = str("Cocktographical library for XChat")
 
 events = ("Channel Message","Private Message", "Private Message to Dialog")
 
@@ -27,12 +27,9 @@ STOP = dechoder_ring["in"]["stop"]
 CONT = dechoder_ring["in"]["cont"]
 MARK = dechoder_ring["in"]["mark"]
 
-buffer = {}
+RE_cocks = re.compile(r"({}|{}).*({}|{})".format(START, MARK, STOP, CONT))
 
-single_line_re = re.compile(r"{}(.*){}".format(START, STOP))
-multi_line_start = re.compile(r"{}(.*){}".format(START, CONT))
-multi_line_cont = re.compile(r"{}(.*){}".format(MARK, CONT))
-multi_line_end = re.compile(r"{}(.*){}".format(MARK, STOP))
+buffer = {}
 
 def to_unicode(strng):
     return(strng)
@@ -47,24 +44,39 @@ def chunks(s, n):
     for start in range(0, len(s), n):
         yield s[start:start+n]
 
+def break_lines(text, split_at=340):
+    '''text should be a space-separated string of unbreakable symbols
+    returns
+        lines: list of space-separated strings, shorter than split_at
+    '''
+    lines = []
+    current_line = []
+    for symb in text.split(" "):
+        new_line = " ".join(current_line+[symb])
+        if len(new_line) > split_at:
+            lines.append(" ".join(current_line))
+            current_line = []
+        current_line.append(symb)
+    lines.append(" ".join(current_line))
+    return lines
 
 
-def enchoder(text, split_at=50):
+def enchode(text, split_at=340):
     text = to_unicode(text)
     first_pass = base64.encodestring(text).replace("\n", "")
     mime_encoded = base64.encodestring(first_pass).replace("\n", "")
 
-    cockstring = [dechoder_ring["in"][c] for c in mime_encoded]
+    cockstring = " ".join([dechoder_ring["in"][c] for c in mime_encoded])
     if len(cockstring) < split_at:
-        cockstring = " ".join(cockstring)
         return(" ".join([START, cockstring, STOP]))
     else:
-        lines = [" ".join(l) for l in chunks(cockstring, split_at)]
+        lines = break_lines(cockstring, split_at)
         sep = " {}\n{} ".format(CONT, MARK)
         ret = "{} {} {}".format(START, sep.join(lines), STOP)
         return(ret)
 
-def dechoder(text):
+
+def dechode(text):
     text = to_unicode(text)
     # decodes a message using the dechoder ring
     dechoded = "".join([dechoder_ring["out"][w] for w in text.split()
@@ -77,44 +89,55 @@ def dechoder(text):
         decoded = base64.decodestring(first_pass)
     except:
         return(first_pass)
-    return(decoded)
+    return(to_unicode(decoded.encode('string_escape')))
 
-def cocktography_cb(word, word_eol, userdata, attr):
-    if re.search(r"{}.*{}".format(START,STOP),word[1]):
-        xchat.emit_print("Channel Message",'\0034\002\037' + word[0] + '\0034\002\037',dechoder(word[1]),"")
-        return xchat.EAT_xchat
-    if re.search(r"{}.*{}".format(START,CONT),word[1]):
-        buffer[word[0]] = word[1]
-        return xchat.EAT_xchat
-    if re.search(r"{}.*{}".format(MARK,CONT),word[1]):
-        buffer[word[0]] = buffer[word[0]] + ' ' + word[1]
-        return xchat.EAT_xchat
-    if re.search(r"{}.*{}".format(MARK,STOP),word[1]):
-        buffer[word[0]] = buffer[word[0]] + ' ' + word[1]
-        xchat.emit_print("Channel Message",'\0034\002\037' + word[0] + '\0034\002\037',dechoder(buffer[word[0]]),"")
-        del buffer[word[0]]
-        return xchat.EAT_xchat
-    return xchat.EAT_NONE
 
+def cocktography_cb(word, word_eol, userdata):
+    text = RE_cocks.search(word[1])
+    if not text:
+        return hexchat.EAT_NONE
+    message = text.group(0)
+
+    if message.startswith(START) or message.startswith(MARK):
+        history = ''
+        if word[0] in buffer:
+            history = buffer[word[0]]
+        if message.endswith(STOP):
+            if message.startswith(START): # we have a single line enchoded message
+                dechoded = dechode(message)
+                formatted = RE_cocks.sub(dechoded, word[1])
+                hexchat.emit_print("Channel Message",'\0034\002\037' + word[0] + '\0034\002\037',formatted,"")
+                return hexchat.EAT_HEXCHAT
+            else:
+                enchoded = "{} {}".format(history, message) if history else message
+                dechoded = dechode(enchoded)
+                formatted = RE_cocks.sub(dechoded, word[1])
+                del buffer[word[0]]
+                hexchat.emit_print("Channel Message",'\0034\002\037' + word[0] + '\0034\002\037',formatted,"")
+                return hexchat.EAT_HEXCHAT
+        else:
+            buffer[word[0]] = "{} {}".format(history, message) if history else message
+            return hexchat.EAT_HEXCHAT
 
 for event in events:
-    xchat.hook_print_attrs(event, cocktography_cb)
+    hexchat.hook_print(event, cocktography_cb)
 
 def enchode_cb(word, word_eol, userdata):
-    s = enchoder(word_eol[1],50)
+    input = word_eol[1][:150]
+    s = enchode(input)
     buffer["input"] = s.splitlines()
     for dongs in buffer["input"]:
-        xchat.get_context().command('say ' + dongs)
+        hexchat.get_context().command('say ' + dongs)
     del buffer["input"]
-    xchat.emit_print("Channel Message",'\0034\002\037' + xchat.get_info('nick') + '\0034\002\037',word_eol[1],"")
+    hexchat.emit_print("Channel Message",'\0034\002\037' + hexchat.get_info('nick') + '\0034\002\037',input,"")
     
-    return xchat.EAT_NONE
+    return hexchat.EAT_HEXCHAT
 
 def dechode_cb(word, word_eol, userdata):
-    xchat.prnt(dechoder(word[1])) 
-    return xchat.EAT_NONE
+    hexchat.prnt(dechode(word[1])) 
+    return hexchat.EAT_HEXCHAT
 
-xchat.hook_command("enchode",enchode_cb)
-xchat.hook_command("dechode",dechode_cb)
+hexchat.hook_command("enchode",enchode_cb)
+hexchat.hook_command("dechode",dechode_cb)
 
-xchat.prnt(__module_name__ + " version " + __module_version__ + " loaded")
+hexchat.prnt(__module_name__ + " version " + __module_version__ + " loaded")
