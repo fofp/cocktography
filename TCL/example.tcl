68ef01ba58fca1dc69c4b87109625418612adc9a
fidsah <vin.king@gmail.com>
2018-08-14 13:45:57 -0400

    Control Chodes Exposed
    Exposing the Control Chodes to the user, and explaining how to use Control Chodes in example

--- TCL/example.tcl
+++ TCL/example.tcl
@@ -9,9 +9,12 @@ set testString "This is a test"
 #enchode returns a list of enchoded lines
 set wangs [::cocktography::enchode $testString] 
 
+#An enchoded message should start with ::cocktography::start and end with ::cocktography::stop
+if {[string first $::cocktography::start $wangs] && [string last $::cocktography::stop $wangs]} {
 #if there's only one line, we can dechode it directly
 set dewanged [::cocktography::dechode [lindex $wangs 0]]
 puts "Input: $testString Enchoded: $wangs dechoded: $dewanged"
+}
 
 
 # This is likely how it would be implemented in the real world for message transmission.
@@ -23,6 +26,17 @@ foreach dong $dongs {
 	puts "Line: $dong"
 
 	#This simulates receiving an enchoded message. 
+	#
+	#Proper message handling should expect to see a message come in as:
+	#
+	#::cocktography::start message ::cocktography::stop
+	#
+	# OR the following format
+	# 
+	#::cocktography::start message ::cocktography::cont
+	#::cocktography::mark message ::cocktography::cont  <-- any number of these between start and stop
+	#::cocktography::mark message ::cocktography::stop
+	
 	append test " " $dong
 }
 
