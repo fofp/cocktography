9ba12598df2d63511dd9b54356eb069f045f2cdd
Tefad <42528416+Tefad@users.noreply.github.com>
2018-09-21 20:43:46 -0400

    More functionality

--- cpi/python/cpi.py
+++ cpi/python/cpi.py
@@ -1,11 +1,13 @@
-
 import itertools
 import os
 import random
+import re
 from enum import Enum
 
+class InvalidCockstring(Exception):
+    pass
 
-def anonymous(cls):
+def _anonymous(cls):
     return cls()
 
 class CyphallicMethod(Enum):
@@ -44,22 +46,36 @@ class Cocktography(object):
             self._digram_to_chode = f.read().splitlines()
         self._kontol_to_chode = {name: chode
                                  for chode, name
-                                 in self._kontol_from_chode.iteritems()}
+                                 in self._kontol_from_chode.items()}
         self._unigram_from_chode = {chode: unigram
                                     for unigram, chode
                                     in enumerate(self._unigram_to_chode)}
         self._digram_from_chode = {chode: digram
                                    for digram, chode
                                    in enumerate(self._digram_to_chode)}
-        @anonymous
+        @_anonymous
         class KONTOL_CHODES:
             START = self._kontol_to_chode["START"]
-            STOP = self._kontol_to_chode["STOP"]
-            MARK = self._kontol_to_chode["MARK"]
-            CONT = self._kontol_to_chode["CONT"]
+            STOP  = self._kontol_to_chode["STOP"]
+            MARK  = self._kontol_to_chode["MARK"]
+            CONT  = self._kontol_to_chode["CONT"]
+            FROM_COCKBLOCK_TYPE = {
+                CockblockType.singleton: (START, STOP),
+                CockblockType.initial: (START, CONT),
+                CockblockType.intermediate: (MARK, CONT),
+                CockblockType.final: (MARK, STOP),
+            }
+            TO_COCKBLOCK_TYPE = {chodes: type
+                                 for type, chodes
+                                 in FROM_COCKBLOCK_TYPE.items()}
+            BEGINNING = (START, MARK)
+            ENDING = (CONT, STOP)
         self.KONTOL_CHODES = KONTOL_CHODES
+        self._RE_COCKBLOCKS = re.compile(r"({}).*({})".format(
+                r"|".join(map(re.escape, KONTOL_CHODES.BEGINNING)),
+                r"|".join(map(re.escape, KONTOL_CHODES.ENDING))))
 
-    def _chodes2bytes(self, chodes):
+    def _chodes2bytes(self, chodes, tolerant=True):
         result = bytearray()
         for chode in chodes:
             if chode in self._unigram_from_chode:
@@ -68,9 +84,11 @@ class Cocktography(object):
                 digram = self._digram_from_chode[chode]
                 result.append(digram >> 8)
                 result.append(digram & 0xFF)
+            elif not tolerant:
+                raise InvalidCockstring("Unknown symbol: {}".format(chode))
         return result
 
-    def _bytes2chodes(self, byte_input, mode):
+    def _bytes2chodes(self, byte_input, mode, varied_unigram_chance = 0.5):
         result = list()
         if mode == CyphallicMethod.unigram:
             for byte in byte_input:
@@ -88,7 +106,7 @@ class Cocktography(object):
         elif mode == CyphallicMethod.varied:
             prev = None
             for byte in byte_input:
-                if random.choice([True, False]): #to unigram or not to unigram
+                if random.random() < varied_unigram_chance:
                     if prev is None:
                         result.append(self._unigram_to_chode[byte])
                     else:
@@ -109,3 +127,13 @@ class Cocktography(object):
 
     def chodes2bytes(self, chodes):
         return self._chodes2bytes(chodes.split())
+
+    def find_cockblock(self, byte_input):
+        match = self._RE_COCKBLOCKS.search(byte_input)
+        if match:
+            groups = match.groups()
+            return (match.start(),
+                    match.end(),
+                    self.KONTOL_CHODES.TO_COCKBLOCK_TYPE[groups])
+        else:
+            return None
--- implementations/test-python/test.py
+++ implementations/test-python/test.py
@@ -6,4 +6,11 @@ print cocktographic.bytes2chodes(b"Test sentence for the widechode edition of co
 print cocktographic.KONTOL_CHODES.STOP
 print cocktographic.chodes2bytes("BuwD; B=D, 8mD; BmD~ B=D` B=D`~ 8nD 8mD;` 8nD' BuD 8nD 8nD`~ BnuD BunD 8uD~, 8nD 8wD; 8uD; 8mD,` 8nD` 8mD~, BmD; BnD~; 8wD;'; BwD', BmwD 8unD' Bw=D BnD, B=D~ 8wD'~ BnD'` 8uD'")
 print cocktographic.chodes2bytes("8=wm=D 8wD~ 8=D B=D, 8mD; 8=D 8unD 8=D 8w=D~ 8mD'; 8mD 8mD;` 8nD' 8wD 8=D; 8mD 8=D~~ 8==D~ BnuD 8=D~~~ 8wD~~~ 8==D 8===D 8nD 8wD; 8==D~ BwD' 8==D 8w=D~ BmD, 8===D~~~ BmD; 8==D 8=D~~~ 8m=D~~ BmD' 8===D~~ 8m==D~ 8w=D 8w===D~ 8wD~~~ Bw=D 8mD 8===D~~~~ B=D~ 8=mD 8w=D BnD'` 8w=D~ 8===D~~ 8=mw=D")
-exit
+text = "PINGAS 8=wm=D 8wD~ 8=D B=D, 8mD; 8=D 8unD 8=D 8w=D~ 8mD'; 8mD 8mD;` 8nD' 8wD 8=D; 8mD 8=D~~ 8==D~ BnuD 8=D~~~ 8wD~~~ 8==D 8===D 8nD 8wD; 8==D~ BwD' 8==D 8w=D~ BmD, 8===D~~~ BmD; 8==D 8=D~~~ 8m=D~~ BmD' 8===D~~ 8m==D~ 8w=D 8w===D~ 8wD~~~ Bw=D 8mD 8===D~~~~ B=D~ 8=mD 8w=D BnD'` 8w=D~ 8===D~~ 8=mw=D FLARP!!!"
+(begin, end, type) = cocktographic.find_cockblock(text)
+print text
+print type
+if type == cpi.CockblockType.singleton:
+    print text[:begin]
+    print text[begin:end]
+    print text[end:]
