251061a0b0c9954b8e0b71a9ebfad5bd094cab38
Tefad <42528416+Tefad@users.noreply.github.com>
2018-10-24 23:46:24 -0400

    HexChat!

--- cpi/python/cpi.py
+++ cpi/python/cpi.py
@@ -1,5 +1,4 @@
 import base64
-# import itertools # check this?
 import os
 import random
 import re
--- implementations/weechat-python/dechoder.py
+++ implementations/weechat-python/dechoder.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 import re
-from cpi import cpi
-
 try:
     import weechat
 except:
     raise Exception(b'This module must be used with Weechat!')
 if __name__ == b'__main__' and __package__ is None:
     from os import sys, path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    sys.path.append(path.dirname(path.abspath(__file__)))
+from cpi import cpi
 
 PREFIX_SEPARATOR = b'\t'
 ZERO_STROKE_FORMAT = b'{strokes}🐓 {prefix}' + \
@@ -27,9 +26,6 @@ __COCKS = {}
 def autococktography(data, modifier, modifier_data, string):
     global api, __COCKS
 
-    def select_format_string(strokes):
-        return MULTI_STROKE_FORMAT if strokes > 0 else ZERO_STROKE_FORMAT
-
     if b'irc_raw' in modifier_data or PREFIX_SEPARATOR not in string:
         return (string)
     prefix, message = string.split(PREFIX_SEPARATOR, 1)
@@ -40,16 +36,17 @@ def autococktography(data, modifier, modifier_data, string):
         return (string)
     if not ALLOW_MIXED_SECURITY and cb_begin != 0 and cb_end != len(message):
         return (string)
-    user = RE_HOST.search(modifier_data)
-    user = user.group(0) if user else b'null'
+    cock_key = RE_HOST.search(modifier_data)
+    cock_key = cock_key.group(0) if cock_key else b'null'
     cyphallic_text = message[cy_begin:cy_end]
     decyphallicized_text = api.decyphallicize(cyphallic_text)
 
     def result(text):
         destroked_text, strokes = api.destroke(text)
+        msg_format = MULTI_STROKE_FORMAT if strokes > 0 else ZERO_STROKE_FORMAT
         return weechat.hook_modifier_exec(
             b'irc_color_decode', b'1',
-            select_format_string(strokes).format(
+            msg_format.format(
                 strokes=strokes,
                 prefix=prefix,
                 cockstring=text,
@@ -59,18 +56,18 @@ def autococktography(data, modifier, modifier_data, string):
     if cb_type == cpi.CockblockType.SINGLETON:
         return result(decyphallicized_text)
     elif cb_type == cpi.CockblockType.INITIAL:
-        __COCKS[user] = decyphallicized_text
+        __COCKS[cock_key] = decyphallicized_text
         return b''
     elif cb_type == cpi.CockblockType.INTERMEDIATE:
-        if user in __COCKS:
-            __COCKS[user] = __COCKS[user] + decyphallicized_text
+        if cock_key in __COCKS:
+            __COCKS[cock_key] = __COCKS[cock_key] + decyphallicized_text
             return b''
         else:
             return (string)
     elif cb_type == cpi.CockblockType.FINAL:
-        if user in __COCKS:
-            msg = __COCKS[user] + decyphallicized_text
-            del __COCKS[user]
+        if cock_key in __COCKS:
+            msg = __COCKS[cock_key] + decyphallicized_text
+            del __COCKS[cock_key]
             return result(msg)
         else:
             return (string)
@@ -89,7 +86,7 @@ def enchoder_cmd(data, buffer, args):
     #  m: mixed chodes
     #  t: thin chodes
     #  w: wide chodes
-    cmd = '/say '
+    cmd = b'/say '
     flags = {b'strokes': 2, 'mode': b't', b'length': 340}
     modes = {
         b'm': cpi.CyphallicMethod.MIXED_CHODE,
@@ -110,7 +107,7 @@ def enchoder_cmd(data, buffer, args):
     if args[0:2] == b'//':
         args = args[1:]
     elif args[0] == b'/':
-        if args[0:4] == b'/me ':  # case insensitive
+        if args[0:4] == b'/me ':
             args = args[4:]
             cmd = b'/me '
         else:
@@ -120,7 +117,7 @@ def enchoder_cmd(data, buffer, args):
             return weechat.WEECHAT_RC_ERROR
     text = api.enchode(
         bytearray(args), flags[b'strokes'], modes[flags[b'mode']],
-        flags['length'])
+        flags[b'length'])
     text = weechat.hook_modifier_exec(b'irc_color_decode', b'1', text)
     for line in text.splitlines():
         weechat.command(buffer, cmd + line)
