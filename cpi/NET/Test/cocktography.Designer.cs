224b5fff82a1d9784e8ce9ce92a0b72117625091
fidsah <vin.king@gmail.com>
2019-07-08 13:18:02 -0400

    .NET CPI and Test Harness
    Initial .NET release

---
+++ cpi/NET/Test/cocktography.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Cocktography
+{
+    partial class cocktography
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.btnEnchode = new System.Windows.Forms.Button();
+            this.btnDechode = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 12);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(859, 270);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "Enchode me!";
+            // 
+            // btnEnchode
+            // 
+            this.btnEnchode.Location = new System.Drawing.Point(714, 315);
+            this.btnEnchode.Name = "btnEnchode";
+            this.btnEnchode.Size = new System.Drawing.Size(75, 23);
+            this.btnEnchode.TabIndex = 1;
+            this.btnEnchode.Text = "Enchode";
+            this.btnEnchode.UseVisualStyleBackColor = true;
+            this.btnEnchode.Click += new System.EventHandler(this.btnEnchode_Click);
+            // 
+            // btnDechode
+            // 
+            this.btnDechode.Location = new System.Drawing.Point(796, 315);
+            this.btnDechode.Name = "btnDechode";
+            this.btnDechode.Size = new System.Drawing.Size(75, 23);
+            this.btnDechode.TabIndex = 2;
+            this.btnDechode.Text = "Dechode";
+            this.btnDechode.UseVisualStyleBackColor = true;
+            this.btnDechode.Click += new System.EventHandler(this.btnDechode_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 362);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Output.Size = new System.Drawing.Size(859, 270);
+            this.Output.TabIndex = 3;
+            this.Output.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 635);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 635);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Strokes:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mixed",
+            "Wide",
+            "Thin"});
+            this.comboBox1.Location = new System.Drawing.Point(268, 317);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mode";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(64, 311);
+            this.trackBar1.Maximum = 13;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(139, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Strokes:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 320);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Mixed Chance:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(518, 317);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "50";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // cocktography
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 657);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.btnDechode);
+            this.Controls.Add(this.btnEnchode);
+            this.Controls.Add(this.Input);
+            this.Name = "cocktography";
+            this.Text = "Cocktography";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Button btnEnchode;
+        private System.Windows.Forms.Button btnDechode;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ cpi/NET/Test/cocktography.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Cocktography;
+using System.Windows.Forms;
+using fofp;
+
+namespace Cocktography
+{
+    public partial class cocktography : Form
+    {
+
+        private cpi.CockMode _chodemode = cpi.CockMode.MIXED_CHODE;
+        private int _strokes = 2;
+        private int _variance = 50;
+        cpi cpi = new cpi();
+        public cocktography()
+        {
+            
+            InitializeComponent();
+            comboBox1.SelectedItem = "Mixed";
+        }
+
+        private void btnEnchode_Click(object sender, EventArgs e)
+        {
+            string[] dongs = cpi.Enchode(Input.Text, _strokes, mode: _chodemode);
+            Output.Clear();
+            Output.Text = string.Join(Environment.NewLine, dongs);
+        }
+
+        private void btnDechode_Click(object sender, EventArgs e)
+        {
+            var fart = Input.Text.Replace(Environment.NewLine, " ");
+
+            var butt = cpi.Dechode(fart);
+            if (butt != null)
+            {
+                label1.Text = butt[0];
+                Output.Text = butt[1];
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedItem.ToString())
+            {
+                case "Wide":
+                    textBox1.Enabled = false;
+                    _chodemode = cpi.CockMode.WIDE_CHODE;
+                    break;
+                case "Thin":
+                    textBox1.Enabled = false;
+                    _chodemode = cpi.CockMode.THIN_CHODE;
+                    break;
+                case "Mixed":
+                    textBox1.Enabled = true;
+                    _chodemode = cpi.CockMode.MIXED_CHODE;
+                    break;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            _strokes = trackBar1.Value;
+            label5.Text = trackBar1.Value.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                textBox1.Text = "50";
+            }
+            _variance = Convert.ToInt32(textBox1.Text);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+}
